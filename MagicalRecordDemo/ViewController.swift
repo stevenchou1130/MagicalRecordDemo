3053a12a3290f55af0ca3befdad5abbbb96387cb
Steven C <steven.artemis@gmail.com>
2021-03-11 11:10:37 +0800

    First Commit

---
+++ MagicalRecordDemo/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  MagicalRecordDemo
+//
+//  Created by Steven on 2021/3/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .yellow
+    }
+}
